def8eda2f0fc97339b9c79d60f4adeb54eae9bd3
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2023-07-03 00:18:30 +0530

    205. Adding a "New Item" Screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,36 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Groceries"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => NewItem(),
+                ),
+              );
+            },
+            icon: Icon(
+              Icons.add,
+            ),
+          )
+        ],
       ),
       body: ListView.builder(
           itemCount: groceryItems.length,
---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a new item"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Text("The form"),
+      ),
+    );
+  }
+}
